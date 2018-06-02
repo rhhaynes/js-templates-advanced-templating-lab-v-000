b23456e6f9ad46160d3fa441e473d120d0396819
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-02 15:39:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ function updateRecipe(){
     description: document.getElementsByName('description')[0].value,
     ingredients: []
   };
-  debugger;
   for (let i=0; i<5; i++){recipe.ingredients.push( document.getElementsByName('ingredients')[i].value )}
   const template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
   document.getElementById('main').innerHTML = template(recipe);
